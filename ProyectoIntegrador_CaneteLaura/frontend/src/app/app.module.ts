5730c4466208e72e6ccf8b9c0e490d524b4db064
lauracanete <109248893+lauracanete@users.noreply.github.com>
2022-09-29 20:47:19 -0300

    seguimos

--- ProyectoIntegrador_CaneteLaura/frontend/src/app/app.module.ts
+++ ProyectoIntegrador_CaneteLaura/frontend/src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './social/social.component';
+import { BanerComponent } from './components/baner/baner.component';
+import { AcercademiComponent } from './components/acercademi/acercademi.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent
+    LogoAPComponent,
+    SocialComponent,
+    BanerComponent,
+    AcercademiComponent,
+  
+    
+    
   ],
   imports: [
     BrowserModule,
