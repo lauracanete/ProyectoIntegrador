bf83b4378553a690a178a7f041389342510a1348
lauracanete <109248893+lauracanete@users.noreply.github.com>
2022-10-12 19:35:10 -0300

    noche

--- ProyectoIntegrador_CaneteLaura/backend/src/main/java/com/portfolio/LauraCanete/Controller/PersonaController.java
+++ ProyectoIntegrador_CaneteLaura/backend/src/main/java/com/portfolio/LauraCanete/Controller/PersonaController.java
@@ -5,6 +5,7 @@ import com.portfolio.LauraCanete.Interface.IPersonaService;
 import java.util.List;
 import static org.hibernate.criterion.Projections.id;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,15 +17,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:8080")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
+    
     
     @GetMapping("/personas/traer")
     public List<Persona> getPersona(){
         return ipersonaService.getPersona();
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona){
         ipersonaService.savePersona(persona);
---
+++ ProyectoIntegrador_CaneteLaura/backend/src/main/java/com/portfolio/LauraCanete/Security/Controller/Mensaje.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.LauraCanete.Security.Controller;
+
+/**
+ *
+ * @author I-MAG
+ */
+public class Mensaje {
+    private String mensaje;
+    
+    //constructor
+
+    public Mensaje() {
+    }
+
+    public Mensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    //Getter and Setter
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+}
