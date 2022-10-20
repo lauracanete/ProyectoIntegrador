b431b7bd3cd3e84a5b3ecdf7e95dced21e7f638d
lauracanete <109248893+lauracanete@users.noreply.github.com>
2022-10-20 19:42:55 -0300

    button

--- ProyectoIntegrador_CaneteLaura/frontend/src/app/app.module.ts
+++ ProyectoIntegrador_CaneteLaura/frontend/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { interceptProvider } from './service/interceptorservice';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia/new-experiencia.component';
 
 
 
@@ -37,6 +38,7 @@ import { interceptProvider } from './service/interceptorservice';
     PiedepaginaComponent,
     HomeComponent,
     LoginComponent,
+    NewExperienciaComponent,
   
     
     
