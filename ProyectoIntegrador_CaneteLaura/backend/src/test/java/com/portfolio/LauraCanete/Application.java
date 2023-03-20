a6df884da605541f599c211a14bd68f84b6641d1
lauracanete <109248893+lauracanete@users.noreply.github.com>
2023-03-20 12:15:32 -0300

    sorpresa
    no funciona nada

--- ProyectoIntegrador_CaneteLaura/backend/src/test/java/com/portfolio/LauraCanete/Application.java
+++ ProyectoIntegrador_CaneteLaura/backend/src/test/java/com/portfolio/LauraCanete/Application.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LauraCaneteApplicationTests {
+        class LauraCaneteApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void Loads() {
 	}
 
 }
